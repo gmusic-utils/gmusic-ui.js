0a624c148238afa741ba18915fe739e667592fc1
Samuel Attard <samuel.r.attard@gmail.com>
2016-06-02 16:02:17 +1000

    Search for track path aswell

--- src/Structs/Playlist.js
+++ src/Structs/Playlist.js
@@ -1,8 +1,22 @@
 import Track from './Track';
 
+let songArrayPath;
+
 export default class Playlist {
   static fromPlaylistObject = (id, playlistObject) => {
-    const playlist = new Playlist(id, playlistObject.Oh.replace(/ playlist$/g, ''));
+    const playlist = new Playlist(id, playlistObject.getTitle().replace(/ playlist$/g, ''));
+    if (playlistObject.items.length > 0 && !songArrayPath) {
+      Object.keys(playlistObject.items[0]).forEach((trackKey) => {
+        if (typeof playlistObject.items[0][trackKey] === 'object') {
+          Object.keys(playlistObject.items[0][trackKey]).forEach((trackArrKey) => {
+            if (Array.isArray(playlistObject.items[0][trackKey][trackArrKey])) {
+              songArrayPath = [trackKey, trackArrKey];
+            }
+          });
+        }
+      });
+    }
+    if (!songArrayPath) return playlist;
     playlist.addTracks(playlistObject.items.map((track) => Track.fromTrackArray(track.Pf.Lc, track.index)));
     return playlist;
   };
